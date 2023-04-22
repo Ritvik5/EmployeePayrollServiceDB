8fb8dc9f4978156ff16848fcbce9830ab0f4c941
Ritvik5 <sharma.ritvik56@gmail.com>
2023-04-22 09:15:50 +0530

    Welcome to ADO.NET

---
+++ EmployeePayrollServiceDB/EmployeeRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EmployeePayrollServiceDB
+{
+    public class EmployeeRepo
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Payroll;Integrated Security=true";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+
+}
